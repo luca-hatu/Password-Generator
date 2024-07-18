8ac4cae99b45671b8913255a9c9bc24559606246
Luca-Matei HATU <lucamhatu@gmail.com>
2024-07-18 19:45:00 +0200

    enhanced the user experience

--- password_generator.py
+++ password_generator.py
@@ -3,6 +3,8 @@ import string
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from ttkbootstrap import Style
+from PIL import Image, ImageTk
 
 def get_user_preferences():
     length = length_var.get()
@@ -55,7 +57,13 @@ def on_generate_password():
     except ValueError as e:
         messagebox.showerror("Error", str(e))
 
-root = tk.Tk()
+def resize_image(path, size):
+    image = Image.open(path)
+    image = image.resize(size)
+    return ImageTk.PhotoImage(image)
+
+style = Style(theme="darkly")
+root = style.master
 root.title("Password Generator")
 
 length_var = tk.IntVar()
@@ -65,32 +73,36 @@ digits_var = tk.BooleanVar()
 special_var = tk.BooleanVar()
 result_var = tk.StringVar()
 
-style = ttk.Style()
-style.configure('TLabel', font=('Helvetica', 12))
-style.configure('TButton', font=('Helvetica', 12))
-style.configure('TEntry', font=('Helvetica', 12))
-style.configure('TCheckbutton', font=('Helvetica', 12))
+style.configure('TLabel', foreground='white')
+style.configure('TButton', foreground='white')
+style.configure('TCheckbutton', foreground='white')
+style.configure('TEntry', foreground='white')
 
-mainframe = ttk.Frame(root, padding="10 10 10 10")
+
+mainframe = ttk.Frame(root, padding="10 10 10 10", style="primary.TFrame")
 mainframe.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-ttk.Label(mainframe, text="Length:").grid(column=0, row=0, sticky=tk.W)
-length_entry = ttk.Entry(mainframe, textvariable=length_var)
+icon_size = (20, 20)
+icon_generate = resize_image("generate.png", icon_size)  
+
+
+ttk.Label(mainframe, text="Length:", compound="left", style="secondary.TLabel").grid(column=0, row=0, sticky=tk.W)
+length_entry = ttk.Entry(mainframe, textvariable=length_var, style="info.TEntry")
 length_entry.grid(column=1, row=0, sticky=(tk.W, tk.E))
 length_entry.focus()
 
-ttk.Checkbutton(mainframe, text="Include Uppercase", variable=uppercase_var).grid(column=0, row=1, sticky=tk.W)
-ttk.Checkbutton(mainframe, text="Include Lowercase", variable=lowercase_var).grid(column=0, row=2, sticky=tk.W)
-ttk.Checkbutton(mainframe, text="Include Digits", variable=digits_var).grid(column=0, row=3, sticky=tk.W)
-ttk.Checkbutton(mainframe, text="Include Special Characters", variable=special_var).grid(column=0, row=4, sticky=tk.W)
+ttk.Checkbutton(mainframe, text="Include Uppercase", variable=uppercase_var, style="success.TCheckbutton").grid(column=0, row=1, sticky=tk.W)
+ttk.Checkbutton(mainframe, text="Include Lowercase", variable=lowercase_var, style="success.TCheckbutton").grid(column=0, row=2, sticky=tk.W)
+ttk.Checkbutton(mainframe, text="Include Digits", variable=digits_var, style="success.TCheckbutton").grid(column=0, row=3, sticky=tk.W)
+ttk.Checkbutton(mainframe, text="Include Special Characters", variable=special_var, style="success.TCheckbutton").grid(column=0, row=4, sticky=tk.W)
 
-ttk.Button(mainframe, text="Generate Password", command=on_generate_password).grid(column=0, row=5, columnspan=2, pady=10)
+ttk.Button(mainframe, text="Generate Password", command=on_generate_password, image=icon_generate, compound="left", style="warning.TButton").grid(column=0, row=5, columnspan=2, pady=10)
 
-ttk.Label(mainframe, text="Generated Password:").grid(column=0, row=6, sticky=tk.W)
-result_entry = ttk.Entry(mainframe, textvariable=result_var, state='readonly')
+ttk.Label(mainframe, text="Generated Password:", style="secondary.TLabel").grid(column=0, row=6, sticky=tk.W)
+result_entry = ttk.Entry(mainframe, textvariable=result_var, state='readonly', style="info.TEntry")
 result_entry.grid(column=1, row=6, sticky=(tk.W, tk.E))
 
 for child in mainframe.winfo_children(): 
